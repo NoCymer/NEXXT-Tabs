ccbf4d5625847b2220d93e592ff1483bda584533
NoCymer <lobsang46@hotmail.com>
2021-11-05 22:43:26 +0100

    general 1st part

--- js/search.js
+++ js/search.js
@@ -1,11 +1,36 @@
 const searchInput = document.querySelector("#search-input");
 const searchForm = document.querySelector("#search-form");
 const searchButton = document.querySelector("#search-button");
+let searchEnigne = 2;
 
+if(localStorage.getItem("searchEngine") != null) {
+    searchEnigne = localStorage.getItem("searchEngine");
+}
 const search = (input) => {
+    if(localStorage.getItem("searchEngine") != null) {
+        searchEnigne = localStorage.getItem("searchEngine");
+    }
     if(input != "" && input != " ") {
-            url = `https://www.google.com/search?q=${input}`;
-            window.location = url;  
+        let url = "";
+        console.log(searchEnigne)
+        switch(Number(searchEnigne)) {
+            case 0:
+                url = `https://www.google.com/search?q=${input}`;
+                window.location = url;  
+                break;
+            case 1:
+                url = `https://duckduckgo.com/?q=${input}`;
+                window.location = url;  
+                break;
+            case 2:
+                url = `https://www.ecosia.org/search?q=${input}`;
+                window.location = url;  
+                break;
+            case 3:
+                url = `https://www.qwant.com/?q=${input}`;
+                window.location = url;  
+                break;
+        }
     }
 }
 
--- js/settings.js
+++ js/settings.js
@@ -1,31 +1,138 @@
 const settingsBtn = document.querySelector("#settings-button");
 const settingsEl = document.querySelector("#settings-page-wrapper");
+const searchBar = document.querySelector("#search-wrapper");
+const generalNavBtn = document.querySelector("#general-nav-button");
+const backgroundNavBtn = document.querySelector("#background-nav-button");
+const infoNavBtn = document.querySelector("#info-nav-button");
+const generalPage = document.querySelector("#general-page");
+const backgroundPage = document.querySelector("#background-page");
+const infoPage = document.querySelector("#info-page");
+const searchEngine = document.querySelector("#search-engine");
+
+let pageIndex = 0;
 let isShown = false;
+let displaysObj = {};
+
+Array.from(settingsEl.children).forEach(element => {
+    displaysObj[element.id] = element.style.display;
+    element.style.display="none";
+    element.style.visibility="hidden";
+});
+generalPage.style.display="none";
+backgroundPage.style.display="none";
+infoPage.style.display="none";
+
+generalPage.style.visibility="hidden";
+backgroundPage.style.visibility="hidden";
+infoPage.style.visibility="hidden";
+const setSettingsPage = (index) => {
+    switch (index){
+        case 0:
+            generalNavBtn.style.borderBottom="#ffffff solid 1px";
+            backgroundNavBtn.style.borderBottom="";
+            infoNavBtn.style.borderBottom="";
+
+            generalPage.style.display="block";
+            backgroundPage.style.display="none";
+            infoPage.style.display="none";
+
+            generalPage.style.visibility="visible";
+            backgroundPage.style.visibility="hidden";
+            infoPage.style.visibility="hidden";
+            break;
+        case 1:
+            generalNavBtn.style.borderBottom="";
+            backgroundNavBtn.style.borderBottom="#ffffff solid 1px";
+            infoNavBtn.style.borderBottom="";
+
+            generalPage.style.display="none";
+            backgroundPage.style.display="block";
+            infoPage.style.display="none";
+
+            generalPage.style.visibility="hidden";
+            backgroundPage.style.visibility="visible";
+            infoPage.style.visibility="hidden";
+            break;
+        case 2:
+            generalNavBtn.style.borderBottom="";
+            backgroundNavBtn.style.borderBottom="";
+            infoNavBtn.style.borderBottom="#ffffff solid 1px";
+
+            generalPage.style.display="none";
+            backgroundPage.style.display="none";
+            infoPage.style.display="block";
+
+            generalPage.style.visibility="hidden";
+            backgroundPage.style.visibility="hidden";
+            infoPage.style.visibility="visible";
+            break;
+    }
+}
+
 
 const toggleSettings = () => {
     if (isShown){
         settingsEl.style.width = "0%";
         settingsEl.style.height = "0%";
         settingsEl.style.top= "0%";
+        searchBar.style.width = "38%";
+        Array.from(settingsEl.children).forEach(element => {
+            element.style.display="none";
+            element.style.visibility="hidden";
+        });
         setTimeout(()=> {
             settingsEl.style.display="none";
             settingsEl.style.visibility="hidden";
-        },300)
+            
+        },300)   
+        settingsBtn.style.transform="rotate(90deg)";
         isShown = false;
     }
     else if (!isShown){
         settingsEl.style.display="block";
         settingsEl.style.visibility="visible";
+        searchBar.style.width = "34%";
         setTimeout(()=> {
-            settingsEl.style.width = "40%";
+            Array.from(settingsEl.children).forEach(element => {
+                element.style.display=displaysObj[element.id];
+                element.style.visibility="visible";
+            });
+            setSettingsPage(pageIndex);
+        },200)
+        setTimeout(()=> {
+            settingsEl.style.width = "32%";
             settingsEl.style.height = "100%";
             settingsEl.style.top= "50%";
+            settingsBtn.style.transform="rotate(0deg)";
         },10)
-        
         isShown = true;
+        
     }    
 }
 
 settingsBtn.addEventListener("click", () => {
     toggleSettings();
+    
 })
+
+generalNavBtn.addEventListener("click", () => {
+    pageIndex = 0;
+    setSettingsPage(pageIndex);
+})
+backgroundNavBtn.addEventListener("click", () => {
+    pageIndex = 1;
+    setSettingsPage(pageIndex);
+})
+infoNavBtn.addEventListener("click", () => {
+    pageIndex = 2;
+    setSettingsPage(pageIndex);
+})
+let sE;
+if(localStorage.getItem("searchEngine") != null) {
+    sE = Number(localStorage.getItem("searchEngine"));
+}
+searchEngine.value = sE;
+
+searchEngine.onchange = () => {
+    localStorage.setItem("searchEngine",searchEngine.value);
+}
