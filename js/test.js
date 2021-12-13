11ef4525319b9bf7feaffd71793ae84bf08e1ab3
NoCymer <lobsang46@hotmail.com>
2021-12-13 15:55:35 +0100

    removing useless files

--- js/test.js
+++
@@ -1,9 +0,0 @@
-const switchEL = document.querySelector("#google-apps-switch");
-let switchBtn = new switchButton(
-    switchEL,
-    "nigga",
-    false,
-    () => console.log("clicked"),
-    () => console.log("checked"),
-    () => console.log("unchecked")
-);
