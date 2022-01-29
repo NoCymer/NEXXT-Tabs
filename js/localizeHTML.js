dd708a81d8817bcf7bb21ec893ee70f73609ed3f
NoCymer <lobsang46@hotmail.com>
2022-01-29 09:25:29 +0100

    adding select language settings

--- js/localizeHTML.js
+++ js/localizeHTML.js
@@ -1,16 +1,29 @@
 const localREGEX = /__MSG_(\w+)__/g
+const supportedLanguges = ["en", "fr", "es", "vi"]
+
+//checks if the language is supported if yes store it
+const languageStorage = new Storage("language", (supportedLanguges.includes(window.navigator.language) ? window.navigator.language : "en"))
+
 const localizeHtml = () => {
-    let elements = document.querySelector("html");
-    elements = elements.childNodes;
-    elements.forEach(element => {
-        let valStr = String(element.innerHTML);
-        let newVal = valStr.replace(localREGEX,(match, extracted) => {
-            return extracted ? chrome.i18n.getMessage(extracted) : "";
+    let html = document.querySelector("html")
+    fetch(`../_locales/${languageStorage.getValue()}/messages.json`).then((response)=> {
+        return response.json();
+    }).then(jsondata => {
+
+        strHtml = String(html.innerHTML)
+        strHtml = strHtml.replace(localREGEX,(match, extracted) => {
+            return extracted ? jsondata[extracted]["message"] : "";
         })
-        if(newVal != valStr)
-        {
-            element.innerHTML = newVal;
-        }
+        html.innerHTML = strHtml;
     });
 }
 localizeHtml();
+setTimeout(() => {
+    const languageEl = document.querySelector("#language-choice")
+    languageEl.value = languageStorage.getValue()
+    $("#refresh-txt").hide()
+    languageEl.onchange = () => {
+        languageStorage.setValue(languageEl.value)
+        $("#refresh-txt").show()
+    }
+}, 500);
